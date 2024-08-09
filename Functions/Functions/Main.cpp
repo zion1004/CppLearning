75166023a77a67be6435c084d4979a6ef079523b
zion1004 <bicoljoy12@gmail.com>
2024-08-10 03:05:14 +0900

    learning functions
    learning the c++ function syntax

---
+++ Functions/Functions/Main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int Multiply(int a, int b) {
+	return a * b;
+}
+
+void MultiplyAndLog(int a, int b) {
+	int result = Multiply(a, b);
+	std::cout << result << std::endl;
+}
+
+int main() {
+	MultiplyAndLog(3, 2);
+	MultiplyAndLog(8, 5);
+	MultiplyAndLog(90, 45);
+
+	std::cin.get();
+	return 0;
+}

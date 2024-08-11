edcbd86b5e5d1ce9534c942d5580d73f6b943eea
zion1004 <bicoljoy12@gmail.com>
2024-08-12 08:23:36 +0900

    more class syntax and struct
    learn about the public and private keywords and also struct

--- Class/Class/src/Source.cpp
+++ Class/Class/src/Source.cpp
@@ -4,14 +4,27 @@
 
 
 class Player {
-public:
+private: // class member is private by default
 	int x, y;
 	int speed;
 
+public: // and public has to be explicitly stated if you want it to be visible outside
 	void Move(int xa, int ya){
 		x += xa * speed;
 		y += ya * speed;
 	}
+};
+
+/*
+	struct in C++ is just like a c++ class
+	but its members are public by default
+*/
+struct Somethinng {
+public: // struct member is public by default
+	int a, b;
+
+private: // and private has to be explicitly stated if you want it to be not accessible
+	int c;
 };
 
 int main() {

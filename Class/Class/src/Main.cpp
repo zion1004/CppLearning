bb3b2aa1042f717214203d177821f6145aa9da97
zion1004 <bicoljoy12@gmail.com>
2024-08-12 08:39:47 +0900

    more class stuff

--- Class/Class/src/Main.cpp
+++ Class/Class/src/Main.cpp
@@ -3,6 +3,7 @@
 #define LOG(x) std::cout << x << std::endl
 
 
+
 class Player {
 private: // class member is private by default
 	int x, y;
@@ -19,7 +20,7 @@ public: // and public has to be explicitly stated if you want it to be visible o
 	struct in C++ is just like a c++ class
 	but its members are public by default
 */
-struct Somethinng {
+struct Something {
 public: // struct member is public by default
 	int a, b;
 
@@ -27,10 +28,41 @@ private: // and private has to be explicitly stated if you want it to be not acc
 	int c;
 };
 
+class Log {
+public:
+	const int LogLevelError = 0;
+	const int LogLevelWarning = 1;
+	const int LogLevelInfo = 2;
+private:
+	int m_LogLevel = LogLevelInfo; // m_ is just a convention to say that a variable is a member variable
+
+public:
+	void SetLevel(int level){
+		m_LogLevel = level;
+	}
+
+	void Error(const char* msg) {
+		if(m_LogLevel >= LogLevelError)
+			std::cout << "[ERROR]: " << msg << std::endl;
+	}
+	void Warn(const char* msg) {
+		if (m_LogLevel >= LogLevelWarning)
+			std::cout << "[WARNING]: " << msg << std::endl;
+	}
+	void Info(const char* msg) {
+		if (m_LogLevel >= LogLevelInfo)
+			std::cout << "[INFO]: " << msg << std::endl;
+	}
+};
+
 int main() {
 	Player p;
-	p.x = 5;
-
+	
+	Log log;
+	log.SetLevel(log.LogLevelWarning);
+	log.Warn("Hello");
+	log.Info("Hello");
+	log.Error("Hello");
 
 	std::cin.get();
 	return 0;
